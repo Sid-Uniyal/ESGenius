57dfe1e12d94039fd00ceaa2ec1dfa25ea053ed7
Sid <siddharth.uniyal@au.pwc.com>
2023-05-31 06:56:48 +0000

    ESGenius added GenAI core function

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -1,6 +1,22 @@
+!pip install langchain
+!pip install openai
+
 import streamlit as st
 import time
 import subprocess
+
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+from langchain.chains import SimpleSequentialChain
+from langchain.agents import create_csv_agent
+import os
+from langchain.utilities import SerpAPIWrapper
+from langchain.agents import Tool
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+from langchain.prompts import PromptTemplate
 
 st.markdown(
     """
@@ -24,7 +40,20 @@ st.title("Welcome to ESGenius")
 def simulate_processing():
     # Add a delay to simulate processing
     time.sleep(3)
+
     
+def Gen_AI_Core_Engine():
+    os.environ['OPENAI_API_KEY'] = "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"
+    llm=OpenAI(temperature=0.0)
+
+    csv_agent_1=create_csv_agent(llm, 'ESGenius/genai_fact_3.csv', verbose=True)
+    csv_agent_2=create_csv_agent(llm, 'ESGenius/carbon_levers_file.csv', verbose=True)
+
+    overall_chain = SimpleSequentialChain(chains=[csv_agent_1, csv_agent_2], verbose=True)
+    response = overall_chain.run("Find the most poluuting facility. Once we have the facility, pick the most polluting category for each Source Type. Finally, based on Categories found, recommend Actions that I should take.")
+    
+    return response
+
 #def run_python_file(file_path):
 #    result = subprocess.run(["python", file_path], capture_output=True, text=True)
 #    output = result.stdout.strip()
@@ -35,10 +64,12 @@ st.write("ESGenius app will give you the methods to control your organizations c
 if st.button("Generate"):
     # Display a spinner while processing
     with st.spinner("Processing..."):
-        output = subprocess.run(["python", "ESGenius/genai_core_engine.py"], capture_output=True, text=True)
+        #output = subprocess.run(["python", "ESGenius/genai_core_engine.py"], capture_output=True, text=True)
+        output=Gen_AI_Core_Engine()
         simulate_processing()
         st.success("Processing completed")
-        st.code(output, language="python")
+        #st.code(output, language="python")
+        st.write(output)
         
 
 # Add more content
