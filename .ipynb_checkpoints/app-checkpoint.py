bce2595f7d777c06b79475ea8a6b5eab5a9cb8fa
EC2 Default User <ec2-user@ip-172-16-86-36.ap-southeast-2.compute.internal>
2023-06-01 06:30:01 +0000

    CSS Update

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -27,7 +27,6 @@ page_bg_img = """
 [data-testid="stAppViewContainer"]{
 background-color: #000000;
 opacity: 0.9;
-background: repeating-linear-gradient( 45deg, #f4f4f7, #f4f4f7 4.5px, #000000 4.5px, #000000 22.5px );
 }
 </style>
 """
